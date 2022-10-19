aa2ade12e431d2708c53148daa479efac84cc3f0
Alejandro Toledo <altoledo@pichincha.com>
2022-10-19 11:15:47 -0500

    add Hash Map examples

---
+++ src/com/example/MapMain.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain {
+
+    public static void main(String[] args) {
+
+        Map<String, String> personas = new HashMap<>();
+
+        personas.put("12345678H", "Nombre Apellido 1");
+        personas.put("12345678L", "Nombre Apellido 2");
+        personas.put("12345678P", "Nombre Apellido 3");
+
+        System.out.println(personas);
+
+        for(String key : personas.keySet()){
+            System.out.println(key);
+        }
+
+        for(String value : personas.values()){
+            System.out.println(value);
+        }
+
+        for(Map.Entry<String, String> pair : personas.entrySet()){
+            System.out.println(pair.getKey() + " : " + pair.getValue());
+        }
+    }
+}
